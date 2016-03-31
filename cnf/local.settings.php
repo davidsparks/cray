d14876a25b8d85501b81c48e49c19705d5b31ab0
David Sparks <david@davidsparks.codes>
2016-03-31 12:32:44 +1100

    Initial commit - Vagrant file and ansible playbook

---
+++ cnf/local.settings.php
@@ -0,0 +1,10 @@
+<?php
+$databases=array('default' => array(
+  'default' => array(
+    'database' => 'default',
+    'username' => 'default',
+    'password' => 'default',
+    'driver' => 'mysql',
+    'host' => 'localhost',
+  ),
+),);
